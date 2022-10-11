e7bf3093b6d166dc87497480d9c2778ca94e4972
Ivan Nuñez <ivanluis.cba@gmail.com>
2022-10-11 14:31:26 -0300

    Start project

---
+++ github_test.js
@@ -0,0 +1,9 @@
+Feature('github');
+
+Scenario('test something', ({ I }) => {
+  I.amOnPage('https://github.com');
+  I.see('GitHub');
+  I.see("Let's build from here,");
+  I.scrollPageToBottom();
+  I.seeElement("//li[contains(.,'© 2022 GitHub, Inc.')]")
+});
