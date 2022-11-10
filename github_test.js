2f92d2e124e45f8ae79638d3e2fb5aa959aabdce
Ivan Nuñez <ivanluis.cba@gmail.com>
2022-11-09 21:03:10 -0300

    Update GitHub test

--- github_test.js
+++ github_test.js
@@ -3,7 +3,7 @@ Feature('github');
 Scenario('test something', ({ I }) => {
   I.amOnPage('https://github.com');
   I.see('GitHub');
-  I.see("Let's build from here,");
+  I.see('Let’s build from here');
   I.scrollPageToBottom();
   I.seeElement("//li[contains(.,'© 2022 GitHub, Inc.')]")
 });
